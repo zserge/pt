84756d24beb4469f605ba6d4bbb002355c986c4c
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2016-11-29 18:56:24 +0200

    clang formatting

--- pt_test.c
+++ pt_test.c
@@ -6,82 +6,82 @@
  * Test reentrant functions with local continuations
  */
 static void pt_reentrant(struct pt *pt, int *reent, int *state) {
-	(*reent)++;
-	pt_begin(pt);
-	pt_label(pt, 1);
-	if (*state == 0) {
-		*state = 1;
-		return;
-	}
-	pt_label(pt, 2);
-	if (*state == 1) {
-		*state = 2;
-		return;
-	}
-	pt_end(pt);
+  (*reent)++;
+  pt_begin(pt);
+  pt_label(pt, 1);
+  if (*state == 0) {
+    *state = 1;
+    return;
+  }
+  pt_label(pt, 2);
+  if (*state == 1) {
+    *state = 2;
+    return;
+  }
+  pt_end(pt);
 }
 
 static void test_local_continuation() {
-	int reent = 0;
-	int state = 0;
-	struct pt pt = pt_init();
+  int reent = 0;
+  int state = 0;
+  struct pt pt = pt_init();
 
-	pt_reentrant(&pt, &reent, &state);
-	assert(pt_status(&pt) == 1 && reent == 1 && state == 1);
+  pt_reentrant(&pt, &reent, &state);
+  assert(pt_status(&pt) == 1 && reent == 1 && state == 1);
 
-	pt_reentrant(&pt, &reent, &state);
-	assert(pt_status(&pt) == 2 && reent == 2 && state == 2);
+  pt_reentrant(&pt, &reent, &state);
+  assert(pt_status(&pt) == 2 && reent == 2 && state == 2);
 
-	pt_reentrant(&pt, &reent, &state);
-	assert(pt_status(&pt) == -1 && reent == 3 && state == 2);
+  pt_reentrant(&pt, &reent, &state);
+  assert(pt_status(&pt) == -1 && reent == 3 && state == 2);
 
-	pt_reentrant(&pt, &reent, &state);
-	assert(pt_status(&pt) == -1 && reent == 4 && state == 2);
+  pt_reentrant(&pt, &reent, &state);
+  assert(pt_status(&pt) == -1 && reent == 4 && state == 2);
 }
 
 /*
  * Test empty protothread
  */
 static void pt_empty(struct pt *pt, int *reent) {
-	pt_begin(pt);
-	(*reent)++;
-	pt_end(pt);
+  pt_begin(pt);
+  (*reent)++;
+  pt_end(pt);
 }
 static void test_empty() {
-	int reent = 0;
-	struct pt pt = pt_init();
+  int reent = 0;
+  struct pt pt = pt_init();
 
-	pt_empty(&pt, &reent);
-	assert(reent == 1);
-	/* shoud not re-enter */
-	pt_empty(&pt, &reent);
-	assert(reent == 1);
+  pt_empty(&pt, &reent);
+  assert(reent == 1);
+  /* shoud not re-enter */
+  pt_empty(&pt, &reent);
+  assert(reent == 1);
 }
 
 /*
  * Test waiting for certain conditions
  */
 static void pt_wait_ready(struct pt *pt, int *reent, int ready) {
-	pt_begin(pt);
-	(*reent)++;
-	pt_wait(pt, ready);
-	(*reent)++;
-	pt_end(pt);
+  pt_begin(pt);
+  (*reent)++;
+  pt_wait(pt, ready);
+  (*reent)++;
+  pt_end(pt);
 }
 static void test_wait() {
-	int ready = 0;
-	int reent = 0;
-	struct pt pt = pt_init();
+  int ready = 0;
+  int reent = 0;
+  struct pt pt = pt_init();
 
-	pt_wait_ready(&pt, &reent, ready);
-	assert(reent == 1);
-	pt_wait_ready(&pt, &reent, ready);
-	assert(reent == 1);
+  pt_wait_ready(&pt, &reent, ready);
+  assert(reent == 1);
+  pt_wait_ready(&pt, &reent, ready);
+  assert(reent == 1);
 
-	ready = 1;
+  ready = 1;
 
-	pt_wait_ready(&pt, &reent, ready);
-	assert(reent == 2);
+  pt_wait_ready(&pt, &reent, ready);
+  assert(reent == 2);
 }
 
 /*
@@ -89,13 +89,13 @@ static void test_wait() {
  */
 static void pt_yielding(struct pt *pt, int *state) {
   pt_begin(pt);
-	(*state) = 1;
+  (*state) = 1;
   pt_yield(pt);
-	(*state) = 2;
+  (*state) = 2;
   pt_end(pt);
 }
 static void test_yield() {
-	int state = 0;
+  int state = 0;
   struct pt pt1 = pt_init();
   struct pt pt2 = pt_init();
 
@@ -113,95 +113,95 @@ static void test_yield() {
  * Test protothread early termination
  */
 static void pt_exiting(struct pt *pt, int *state) {
-	pt_begin(pt);
-	(*state) = 1;
-	pt_exit(pt, PT_STATUS_FINISHED);
-	(*state) = 2;
-	pt_end(pt);
+  pt_begin(pt);
+  (*state) = 1;
+  pt_exit(pt, PT_STATUS_FINISHED);
+  (*state) = 2;
+  pt_end(pt);
 }
 static void test_exit() {
-	int state = 0;
-	struct pt pt = pt_init();
+  int state = 0;
+  struct pt pt = pt_init();
 
-	pt_exiting(&pt, &state);
-	assert(state == 1 && pt_status(&pt) == PT_STATUS_FINISHED);
-	pt_exiting(&pt, &state);
-	assert(state == 1 && pt_status(&pt) == PT_STATUS_FINISHED);
+  pt_exiting(&pt, &state);
+  assert(state == 1 && pt_status(&pt) == PT_STATUS_FINISHED);
+  pt_exiting(&pt, &state);
+  assert(state == 1 && pt_status(&pt) == PT_STATUS_FINISHED);
 }
 
 /*
  * Test complex protothread wait loops
  */
-static void pt_loop_with_braces(struct pt *pt, int *state, int timeout, int
-		ready) {
+static void pt_loop_with_braces(struct pt *pt, int *state, int timeout,
+                                int ready) {
   pt_begin(pt);
-	*state = 1;
+  *state = 1;
   pt_loop(pt, !timeout) {
     if (ready) {
       break;
     }
-		(*state)++;
+    (*state)++;
   }
-	*state = -1;
+  *state = -1;
   pt_end(pt);
 }
 static void pt_loop_without_braces(struct pt *pt, int *state, int timeout,
-		int ready) {
+                                   int ready) {
   pt_begin(pt);
-	*state = 1;
+  *state = 1;
 // clang-format off
 #ifdef __clang__
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdangling-else"
 #endif
-	if (1)
-		pt_loop(pt, !timeout)
-			if (ready)
-				break;
-			else
-				(*state)++;
+  if (1)
+    pt_loop(pt, !timeout)
+      if (ready)
+	break;
+      else
+	(*state)++;
 #ifdef __clang__
 #pragma clang diagnostic pop
 #endif
   // clang-format on
-	*state = -1;
+  *state = -1;
   pt_end(pt);
 }
 #include <stdio.h>
 static void test_loop() {
-	int state = 0;
-	struct pt pt1 = pt_init();
-	struct pt pt2 = pt_init();
-	struct pt pt3 = pt_init();
+  int state = 0;
+  struct pt pt1 = pt_init();
+  struct pt pt2 = pt_init();
+  struct pt pt3 = pt_init();
 
-	pt_loop_with_braces(&pt1, &state, 0, 0);
-	assert(state == 2);
-	pt_loop_with_braces(&pt1, &state, 0, 0);
-	assert(state == 3);
-	pt_loop_with_braces(&pt1, &state, 1, 0);
-	assert(state == -1);
+  pt_loop_with_braces(&pt1, &state, 0, 0);
+  assert(state == 2);
+  pt_loop_with_braces(&pt1, &state, 0, 0);
+  assert(state == 3);
+  pt_loop_with_braces(&pt1, &state, 1, 0);
+  assert(state == -1);
 
-	pt_loop_without_braces(&pt2, &state, 0, 0);
-	assert(state == 2);
-	pt_loop_without_braces(&pt2, &state, 0, 0);
-	assert(state == 3);
-	pt_loop_without_braces(&pt2, &state, 1, 0);
-	assert(state == -1);
+  pt_loop_without_braces(&pt2, &state, 0, 0);
+  assert(state == 2);
+  pt_loop_without_braces(&pt2, &state, 0, 0);
+  assert(state == 3);
+  pt_loop_without_braces(&pt2, &state, 1, 0);
+  assert(state == -1);
 
-	pt_loop_without_braces(&pt3, &state, 0, 0);
-	assert(state == 2);
-	pt_loop_without_braces(&pt3, &state, 0, 0);
-	assert(state == 3);
-	pt_loop_without_braces(&pt3, &state, 0, 1);
-	assert(state == -1);
+  pt_loop_without_braces(&pt3, &state, 0, 0);
+  assert(state == 2);
+  pt_loop_without_braces(&pt3, &state, 0, 0);
+  assert(state == 3);
+  pt_loop_without_braces(&pt3, &state, 0, 1);
+  assert(state == -1);
 }
 
 int main() {
-	test_local_continuation();
-	test_empty();
-	test_wait();
-	test_yield();
-	test_exit();
-	test_loop();
-	return 0;
+  test_local_continuation();
+  test_empty();
+  test_wait();
+  test_yield();
+  test_exit();
+  test_loop();
+  return 0;
 }
