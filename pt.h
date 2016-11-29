b53d4664ba54a5c69a1ea525698b63d7f1d25f61
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2016-11-29 17:50:35 +0200

    added switch-based local continuation, commended out other tests for now

--- pt.h
+++ pt.h
@@ -1,4 +1,45 @@
 #ifndef PT_H
 #define PT_H
 
+#if PT_USE_SETJMP
+/*
+ * Local continuation that uses setjmp()/longjmp() to keep thread state.
+ *
+ * Pros: local variables are preserved, works with all control structures.
+ * Cons: slow, may erase global register variables.
+ */
+#elif PT_USE_GOTO
+/*
+ * Local continuation based on goto label references.
+ *
+ * Pros: works with all control sturctures.
+ * Cons: requires GCC or Clang, doesn't preserve local variables.
+ */
+#else
+/*
+ * Local continuation based on switch/case and line numbers.
+ *
+ * Pros: works with any C99 compiler, most simple implementation.
+ * Cons: doesn't allow more than one label per line, doesn't preserve local variables.
+ */
+struct pt {
+	int label;
+	int status;
+};
+#define pt_init()                                                              \
+  { .label = 0 }
+#define pt_begin(pt)                                                           \
+  switch ((pt)->label) {                                                       \
+  case 0:
+#define pt_label(pt, stat)                                                     \
+  do {                                                                         \
+    (pt)->label = __LINE__;                                                    \
+    (pt)->status = (stat);                                                     \
+  case __LINE__:;                                                              \
+  } while (0)
+#define pt_end(pt)                                                             \
+  pt_label(pt, -1);                                                            \
+  }
+#endif
+
 #endif /* PT_H */
--- pt_test.c
+++ pt_test.c
@@ -1,5 +1,45 @@
+#include <assert.h>
+
 #include "pt.h"
 
+/*
+ * Test reentrant functions with local continuations
+ */
+static void pt_reentrant(struct pt *pt, int *reent, int *state) {
+	(*reent)++;
+	pt_begin(pt);
+	pt_label(pt, 1);
+	if (*state == 0) {
+		*state = 1;
+		return;
+	}
+	pt_label(pt, 2);
+	if (*state == 1) {
+		*state = 2;
+		return;
+	}
+	pt_end(pt);
+}
+
+static void test_local_continuation() {
+	int reent = 0;
+	int state = 0;
+	struct pt pt = pt_init();
+
+	pt_reentrant(&pt, &reent, &state);
+	assert(pt.status == 1 && reent == 1 && state == 1);
+
+	pt_reentrant(&pt, &reent, &state);
+	assert(pt.status == 2 && reent == 2 && state == 2);
+
+	pt_reentrant(&pt, &reent, &state);
+	assert(pt.status == -1 && reent == 3 && state == 2);
+
+	pt_reentrant(&pt, &reent, &state);
+	assert(pt.status == -1 && reent == 4 && state == 2);
+}
+
 int main() {
+	test_local_continuation();
 	return 0;
 }
